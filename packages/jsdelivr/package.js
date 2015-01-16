e8bcdd76f105b558fbcb7214f56d21b61c7a3db4
Arun <aruntknambiar@gmail.com>
2015-01-17 04:37:26 +0530

    merge

--- packages/jsdelivr/package.js
+++ packages/jsdelivr/package.js
@@ -1,11 +1,11 @@
-    Package.describe({                                                                                                 // 54
-        summary: "jsdelivr CDN container",                                                                             // 55
-        version: "1.0.0",                                                                                              // 56
-        name: "jsdelivr"                                                                                               // 57
-    });                                                                                                                // 58
-                                                                                                                       // 59
-    Package.onUse(function(api) {                                                                                      // 60
-        api.versionsFrom("METEOR@1.0");                                                                                // 61
-        api.use("templating@1.0.8", "client");                                                                         // 62
-        api.add_files(["head.html"], "client");                                                                        // 63
-    });                                                                                                                // 64
+    Package.describe({                                                                                                // 33
+        summary: "jsdelivr CDN container",                                                                            // 34
+        version: "1.0.0",                                                                                             // 35
+        name: "jsdelivr"                                                                                              // 36
+    });                                                                                                               // 37
+                                                                                                                      // 38
+    Package.onUse(function(api) {                                                                                     // 39
+        api.versionsFrom("METEOR@1.0");                                                                               // 40
+        api.use("templating@1.0.8", "client");                                                                        // 41
+        api.add_files(["head.html"], "client");                                                                       // 42
+    });                                                                                                               // 43
